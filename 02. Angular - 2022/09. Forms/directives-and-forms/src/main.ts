33ef49917da915b8c1e829ed9712d3d86f097deb
VladimirovMario <mariovladimirovfs@gmail.com>
2022-11-28 16:36:29 +0200

    Example for abstraction

--- 02. Angular - 2022/09. Forms/directives-and-forms/src/main.ts
+++ 02. Angular - 2022/09. Forms/directives-and-forms/src/main.ts
@@ -10,3 +10,51 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+/*
+// Working with abstractions
+import { Provider } from "@angular/core";
+
+  class Renderer {
+  
+}
+interface IRender<T> {
+  createTextElement(content: string): T;
+}
+
+class DOMRenderer implements IRender<HTMLElement> {
+  private createElement(elementType: string, content: string): HTMLElement {
+    const el = document.createElement(elementType);
+    el.textContent = content;
+    return el;
+  }
+
+  createTextElement(content: string): HTMLElement {
+    return this.createElement('p', content);
+  }
+}
+
+class MDRenderer implements IRender<any> {
+  createTextElement(content: string) {
+    return '```' + content + '```';
+  }
+}
+
+const render = new MDRenderer();//DOMRenderer();
+
+const providers: Provider[] = [
+  {
+    provide: Renderer,
+    useClass: DOMRenderer 
+  }
+];
+
+const providers2: Provider[] = [
+  {
+    provide: Renderer,
+    useClass: MDRenderer
+  }
+]
+
+render.createTextElement('HELLO WORD');
+*/
