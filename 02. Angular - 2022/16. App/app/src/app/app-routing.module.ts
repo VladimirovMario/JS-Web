180c34516a8332a012e8e1901c9dc5fe55900fd9
VladimirovMario <mariovladimirovfs@gmail.com>
2022-12-09 14:36:18 +0200

    TitleStrategy to change title

--- 02. Angular - 2022/16. App/app/src/app/app-routing.module.ts
+++ 02. Angular - 2022/16. App/app/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RouterModule, RouterStateSnapshot, Routes, TitleStrategy } from '@angular/router';
 import { HomeComponent } from './core/home/home.component';
 
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
-import { MainComponent } from './game/main/main.component';
-import { NewGameComponent } from './game/new-game/new-game.component';
+
 
 const routes: Routes = [
   {
@@ -22,20 +22,28 @@ const routes: Routes = [
   },
 ];
 
+@Injectable({
+  providedIn: 'root',
+})
+
+
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(title? `${title}  | Angular Exam`  :  'Angular Exam');
+    }
+  }
+}
+
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [{ provide: TitleStrategy, useClass: TemplatePageTitleStrategy }],
 })
 export class AppRoutingModule {}
-
-/*
-  // {
-    // path: '',
-    // pathMatch: 'full',
-    // component: HomeComponent,
-  // },
-  {
-    // path: 'catalog',
-    // component: MainComponent,
-  // },
-  */
